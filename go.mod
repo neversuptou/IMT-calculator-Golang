2b5ff3e6459bf272b554edc64f7edac1c06931fa
neversuptou <danilasereda2@yandex.ru>
2024-08-17 23:04:28 +0300

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module golang_pet
+
+go 1.23.0
---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const IMTPower = 2
+
+func main() {
+	for {
+		fmt.Println("__ Калькулятор индекса массы тела __ ")
+		userWeight, userHeight := getUserInput()
+		IMT := calculateIMT(userWeight, userHeight)
+		outputResult(IMT)
+		if breakeApp() {
+			continue
+		} else {
+			break
+		}
+	}
+
+}
+
+func outputResult(IMT float64) {
+	switch {
+	case IMT < 16:
+		fmt.Println("У вас сильный дефицит массы тела")
+	case IMT < 18.5:
+		fmt.Println("У вас дефицит массы тела")
+	case IMT < 25:
+		fmt.Println("У вас нормальная масса тела")
+	case IMT < 30:
+		fmt.Println("У вас избыточная масса тела")
+	case IMT < 35:
+		fmt.Println("У вас 1-я степень ожирения")
+	case IMT < 40:
+		fmt.Println("У вас 2-я степень ожирения")
+	default:
+		fmt.Println("У вас 3-я степень ожирения")
+	}
+	result := fmt.Sprintf("Ваш индекс массы тела: %.0f\n", IMT)
+	fmt.Print(result)
+}
+
+func calculateIMT(userWeight float64, userHeight float64) (IMT float64) {
+	IMT = userWeight / math.Pow(userHeight/100, IMTPower)
+	return IMT
+}
+
+func getUserInput() (userHeight float64, userWeight float64) {
+	fmt.Println("Введите свой рост(см):")
+	fmt.Scan(&userHeight)
+	fmt.Println("Введите свой вес:")
+	fmt.Scan(&userWeight)
+	return userWeight, userHeight
+}
+
+func breakeApp() bool {
+	var answerForCalc string
+	fmt.Println("Хотите рассчитать индекс повторно? y/n")
+	fmt.Scan(&answerForCalc)
+	if answerForCalc == "y" || answerForCalc == "Y" {
+		return true
+	}
+	return false
+}
